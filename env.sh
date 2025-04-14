a179c853c73e7073350b45a961cf39efc2a6f23f
Peeter Joot <peeterjoot@pm.me>
2025-04-14 00:03:09 -0400

    new file:   bin/.env.old.sh deleted:    env.sh

--- env.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATH=".:`pwd`/antlr-4.13.2-complete.jar:$CLASSPATH"
-alias antlr4='java -Xmx500M -cp "`pwd`/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java -Xmx500M -cp "`pwd`/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
