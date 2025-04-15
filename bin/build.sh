a65fd52729878aa780e2f4db8700de4af358f8d7
Peeter Joot <peeterjoot@pm.me>
2025-04-14 23:02:03 -0400

    cmake with -Ds again.  re-enable the find_packages

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,9 @@ rm -rf build
 mkdir build
 cd build
 
-cmake -G Ninja ..
+cmake -G Ninja \
+-DLLVM_DIR=/usr/local/llvm-20/lib64/cmake/llvm \
+-DMLIR_DIR=/usr/local/llvm-20/lib64/cmake/mlir \
+..
 
 ninja
