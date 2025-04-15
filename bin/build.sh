26951124cae834b1451ac60adc0b4fa1bb2a9c67
Peeter Joot <peeterjoot@pm.me>
2025-04-14 22:57:43 -0400

    get rid of -DLLVM...

--- bin/build.sh
+++ bin/build.sh
@@ -4,9 +4,6 @@ rm -rf build
 mkdir build
 cd build
 
-cmake -G Ninja \
-      -DLLVM_DIR=/usr/lib/llvm-19/cmake \
-      -DMLIR_DIR=/usr/lib/llvm-19/lib/cmake/mlir \
-      ..
+cmake -G Ninja ..
 
 ninja
