f6668aa27a9f211f6c44302b3f8e2fc90cea0097
Peeter Joot <peeterjoot@pm.me>
2025-04-20 20:38:17 -0400

    Add ToyDialect::initialize (GET_OP_LIST)

--- src/ToyDialect.cpp
+++ src/ToyDialect.cpp
@@ -1,10 +1,18 @@
 #include "ToyDialect.h"
 
+#define GET_OP_CLASSES
+#include "ToyDialect.cpp.inc"
+
 // Include generated dialect definitions
 #include "ToyDialectDefs.cpp.inc"
 
-// Include generated operation definitions
-#define GET_OP_CLASSES
+namespace toy {
+void ToyDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
 #include "ToyDialect.cpp.inc"
+      >();
+}
+} // namespace toy
 
 // vim: et ts=4 sw=4
