2f9ead7dc606e9686e0f8112c8b84e0e0af9286b
Peeter Joot <peeterjoot@pm.me>
2026-02-16 11:32:41 -0500

    ModuleInsertionPointGuard.hpp -- reimplement as a simple mlir::OpBuilder::InsertionGuard wrapper.

--- src/driver/ModuleInsertionPointGuard.hpp
+++ src/driver/ModuleInsertionPointGuard.hpp
@@ -1,41 +1,30 @@
 ///
 /// @file    ModuleInsertionPointGuard.hpp
 /// @author  Peeter Joot <peeterjoot@pm.me>
-/// @brief   This file implements a RAII insertion point save/restore helpers to switch to start of module scope.
+/// @brief   This file implements a RAII insertion point save/restore helper to switch to start of module scope.
 ///
 #pragma once
 
-#include <mlir/IR/BuiltinOps.h>
 #include <mlir/IR/Builders.h>
+#include <mlir/IR/BuiltinOps.h>
 
 namespace silly
 {
-    /// @brief An RAII insertion point save and restoration, setting new IP to module start
-    ///
-    /// This is a RAII helper that temporarily sets the insertion point to the start of a module body
-    /// and restores the original insertion point on scope exit.
+    /// RAII helper to temporarily set insertion point to the start of a module body.
+    /// Uses upstream mlir::OpBuilder::InsertionGuard under the hood for restore.
     class ModuleInsertionPointGuard
     {
        public:
         /// @brief Constructor, that sets the insertion point to the beginning of the module, saving the current IP.
         ///
         /// @param mod Module whose body to insert into
-        /// @param opBuilder Builder to guard
-        ModuleInsertionPointGuard( mlir::ModuleOp& mod, mlir::OpBuilder& opBuilder )
-            : builder{ opBuilder }, oldIP{ builder.saveInsertionPoint() }
-        {
+        /// @param builder Builder to guard
+        ModuleInsertionPointGuard( mlir::ModuleOp& mod, mlir::OpBuilder& builder ) : guard( builder )
+        {    // saves old IP
             builder.setInsertionPointToStart( mod.getBody() );
         }
 
-        /// Destructor, that restores the original insertion point.
-        ~ModuleInsertionPointGuard()
-        {
-            builder.restoreInsertionPoint( oldIP );
-        }
-
        private:
-        mlir::OpBuilder& builder;    ///< cache the builder for IP restoration.
-
-        mlir::OpBuilder::InsertPoint oldIP;    ///< the old IP
+        mlir::OpBuilder::InsertionGuard guard;    ///< restores IP on destruction
     };
-}
+}    // namespace silly
